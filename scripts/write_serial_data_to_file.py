76125dcd76e5749231495b585dd28eeb70214b52
Yuki Asano <asano@jsk.imi.i.u-tokyo.ac.jp>
2021-12-13 15:36:41 +0900

    change port

--- scripts/write_serial_data_to_file.py
+++ scripts/write_serial_data_to_file.py
@@ -4,14 +4,11 @@ import os
 import sys
 import serial
 
-PORT = '/dev/ttyACM1'
-BAUD = 9600
+PORT = '/dev/ttyUSB0'
+BAUD = 57600
 ser = serial.Serial(PORT, BAUD)
 
 f = open('test.txt', 'w')
-
-data_format =['# ', 'elapsed_time ', 'temp' '\n']
-f.writelines(data_format)
 
 try:
     while ser.is_open:
