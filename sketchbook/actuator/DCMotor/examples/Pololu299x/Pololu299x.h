ab57f5e6763425f9110fb596114f5af3b6d3255d
Yuki Asano <yasano@g.ecc.u-tokyo.ac.jp>
2022-12-01 13:57:44 +0900

    generate DCmotor class

---
+++ sketchbook/actuator/DCMotor/examples/Pololu299x/Pololu299x.h
@@ -0,0 +1,59 @@
+// Pololu G2 High-Power Motor Driver 24v13 #2992
+// Pololu G2 High-Power Motor Driver 24v21 #2995
+#include "DCMotor.h"
+
+
+class Pololu299x : public DCMotor {
+ public:
+  uint8_t pin_pwm;
+  uint8_t pin_dir;  // designating rotation direction
+
+ Pololu299x(uint8_t _pin_pwm, uint8_t _pin_dir):
+  DCMotor(_pin_pwm)
+  {
+    pin_pwm = _pin_pwm;
+    pin_dir = _pin_dir;
+  }
+
+  ~Pololu299x() {};
+
+
+  void initialize() {
+    pinMode(pin_pwm, OUTPUT);
+    pinMode(pin_dir, OUTPUT);
+    digitalWrite(pin_pwm, LOW);  // default LOW = disable
+    digitalWrite(pin_dir, LOW);  // default LOW
+  }
+
+  //pin_dir:
+  // HIGH: forward drive (OUTA -> OUTB)
+  // LOW : backward drive(OUTB -> OUTA)
+  void forwardDrive(uint8_t pwm_duty, unsigned int duration) {
+    Serial.println("Driving forward");
+    Serial.print(duration);
+    Serial.println("[ms]");
+
+    digitalWrite(pin_dir, HIGH);  // change direction
+    PWMdutyChange(pwm_duty);
+
+    delay(duration);
+  }
+
+  void backwardDrive(uint8_t pwm_duty, unsigned int duration) {
+    Serial.println("Driving backward");
+    Serial.print(duration);
+    Serial.println("[ms]");
+
+    digitalWrite(pin_dir, LOW);  // change direction
+    PWMdutyChange(pwm_duty);
+
+    delay(duration);
+  }
+
+  void brakeDrive() {
+    Serial.println("Brake");
+
+    digitalWrite(pin_pwm, LOW);
+  }
+
+};
---
+++ sketchbook/actuator/DCMotor/examples/Pololu299x/Pololu299x.ino
@@ -0,0 +1,28 @@
+#include "Pololu299x.h"
+
+#define BAUD 57600
+#define PIN_PWM 3
+#define PIN_DIR 4
+
+
+Pololu299x P(PIN_PWM, PIN_DIR);
+
+void setup() {
+  Serial.begin(BAUD);
+  P.initialize();
+  delay(500);
+}
+
+void loop() {
+  Serial.println("---START LOOP---");
+
+  P.forwardDrive(20, 1000);
+  P.brakeDrive();
+  delay(500);
+
+  P.backwardDrive(20, 1000);
+  P.brakeDrive();
+  delay(500);
+
+  Serial.println("");
+}
